6bce1d0a94047bc617f2461b2d7b7df0aaa7401c
FajariSultan <fajarisultan123@gmail.com>
2021-04-23 14:06:45 -0700

    Operator Binary Assigment

---
+++ src/operator/OperatorBinary.java
@@ -0,0 +1,14 @@
+package operator;
+
+public class OperatorBinary {
+
+    //operator (=) sebagai assigment/penugasan
+    //menempatkan nilai kedalam variable
+    //baris 5-7 adalah comment/komentar yang tidak di jalankan oleh program
+
+    String nama= "Fajari Sultan";
+    int jumlahSks = 24;
+    double indeksPrestasiKumulatif = 3.89;
+
+
+}
