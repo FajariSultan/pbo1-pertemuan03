a4b24800a0e93198ca5fafd1aa0374c4cfe8e58a
FajariSultan <fajarisultan123@gmail.com>
2021-04-24 10:42:53 -0700

    IfElse statement2

--- src/Percabangan/IfElse2.java
+++ src/Percabangan/IfElse2.java
@@ -1,14 +1,14 @@
 package Percabangan;
 
 public class IfElse2 {
-   public static void main(String[] args) {
-       double totalBelanja = 25000;
-       double uangdiDompet = 50000;
+    public static void main(String[] args) {
+        double totalBelanja = 25000;
+        double uangDiDompet = 50000;
 
-       if(uangdiDompet<totalBelanja) {
-           System.out.println("Uang kurang, kurangi jajan anda");
-       }else{
-           System.out.println("Uang cukup, berelaan lah");
-       }
-   }
+        if(uangDiDompet<totalBelanja){
+            System.out.println("uang kurang, kurangi jajan anda");
+        }else{
+            System.out.println("Uang cukup, berelaanlah");
+        }
+    }
 }
